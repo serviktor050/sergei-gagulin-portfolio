438b4b70d05063827c0a9fc4ebf5bb790887791b
Sergei Gagulin <mr.s-gagulin@yandex.ru>
2026-05-18 16:12:08 +0300

    DEV-Setting up eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './app/App';
 import './i18n';
 
